93435b9bc06089ef7dc0237340ef9755d4386d12
irensula <irinawebdev@gmail.com>
2024-01-09 23:07:33 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+// npm i cookie-parser
+const cookieParser = require('cookie-parser'); 
+
+app.use(cookieParser());
 
 app.get('/greet', (req, res) => {
-    res.send('Hey there!');
+    const { name = 'No-name'} = req.cookies;
+    res.send(`Hey there, ${name}!`);
 })
 
 app.get('/setname', (req, res) => {
+    res.cookie('name', 'Henrietta');
     res.cookie('font-family', 'Helvetica');
-    res.cookie('background-color', 'fff')
+    res.cookie('background-color', 'fff');
     res.send('Ok, sent you a cookie!');
 })
 
